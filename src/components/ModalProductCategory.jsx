2ad51bebc1defac8ce7061243c20cc1e788f2e2b
Rizki Susilo Ramadhan <rizkysr90@gmail.com>
2023-01-13 19:08:56 +0700

    add category feat

---
+++ src/components/ModalProductCategory.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+
+export default function ModalProductCategory(props) {
+
+  return (
+    <>
+        <div className='flex items-center justify-center w-full 
+            min-h-screen z-50 fixed left-0 top-0 bg-neutral lg:ml-44 '>
+                <div className='p-8 flex  flex-col items-center bg-base-100 w-2/5'>
+                    <div className='text-primary font-bold text-2xl '>{[props?.header]}</div>
+                    <form className='mt-4'
+                        onSubmit={props.editableData ? (e) => props?.action(e,props.id) : props?.action}
+                    >
+                        {
+                            props.editableData ? 
+                            <input type="text" 
+                            onChange={(e) => props.editableAction(e.target.value)}
+                            value={props.editableData?.name} 
+                            name='name' placeholder="Masukkan Nama Kategori" 
+                            className="input input-bordered w-full max-w-xs" /> :
+                            <input type="text" name='name' placeholder="Masukkan Nama Kategori" 
+                            className="input input-bordered w-full max-w-xs" />
+                        }
+                        <div className='flex justify-center mt-4'>
+                            <button type='reset' className='btn btn-warning mr-4'
+                            onClick={() => props.isModalOpen(false)}
+                            >Batal</button>
+                            {
+                                 props.editableData ? 
+                                 <button type='submit' className='btn btn-primary'>Simpan data</button>
+                                 
+                                 :
+                                 <button type='submit' className='btn btn-primary'>Tambah</button>
+                            }
+                        </div>
+                    </form>
+                </div>
+        </div>
+    </>
+  )
+}
--- src/pages/ProductCategories.jsx
+++ src/pages/ProductCategories.jsx
@@ -1,12 +1,188 @@
-import React, { useState } from 'react'
+import axios from 'axios';
+import React, { useState,useEffect } from 'react'
+import { BeatLoader } from 'react-spinners';
+import { toast, ToastContainer } from 'react-toastify';
+import ModalProductCategory from '../components/ModalProductCategory';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPenToSquare, faTrash } from '@fortawesome/free-solid-svg-icons';
 
 export default function ProductCategories() {
-    const [productCategories, setProductCategories] = useState('');
+    const override = {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        position: "absolute",
+        left:"0",
+        top:"0",
+        backgroundColor:"rgba(0,0,0,.3)",
+        width: "100%",
+        zIndex: "99",
+        minHeight: "100%",
+        margin: "0 auto",
+      };
+    const [productCategories, setProductCategories] = useState([]);
+    const [modalAdd, setModalAdd] = useState(false);
+    const [modalUpdate, setModalUpdate] = useState(false);
+    const [productCategory, setProductCategory] = useState({});
+    const [selectedCategoryId, setSelectedCategoryId] = useState('');
+    const [isLoading, setisLoading] = useState(false);
+    const getCategoryById = async (id) => {
+        try {
+            setModalUpdate(true);
+            setisLoading(true);
+            setSelectedCategoryId(id);
+            const res = await axios.get(`${process.env.REACT_APP_API_HOST}/productCategories/${id}`).then((res) => res.data);
+            setProductCategory(res.data);
+            setisLoading(false);
+        } catch (error) {
+            let errMsg = 'Internal Server Error'
+            if (error.response?.status !== 500) {
+                errMsg = error.response?.data?.metadata?.msg
+            }
+            
+            toast.error(`Error ${error?.response?.status} - ${errMsg}`, {
+                position: toast.POSITION.TOP_RIGHT
+            });
+            setisLoading(false);
+
+        }
+    }
+    const handleDeleteCategory = async (id) => {
+        try {
+            setisLoading(true);
+            const res = await axios.delete(`${process.env.REACT_APP_API_HOST}/productCategories/${id}`).then(res => res.data?.metadata);
+            setisLoading(false);
+            toast.success(`${res?.msg}`,{
+                position: toast.POSITION.TOP_RIGHT
+            });
+        } catch (error) {
+            let errMsg = 'Internal Server Error'
+            if (error.response?.status !== 500) {
+                errMsg = error.response?.data?.metadata?.msg
+            }
+            
+            toast.error(`Error ${error?.response?.status} - ${errMsg}`, {
+                position: toast.POSITION.TOP_RIGHT
+            });
+            setisLoading(false);
+        }
+    }
+    const handleSubmitEdit = async (e,id) => {
+        e.preventDefault();
+        setisLoading(true);
+        const form = e.target;
+        const formData = new FormData(form);
+        const formJSON = Object.fromEntries(formData.entries());
+
+        try {
+            const res = await axios.put(`${process.env.REACT_APP_API_HOST}/productCategories/${id}`, formJSON).then(res => res.data.metadata);
+            setisLoading(false);
+            toast.success(`${res?.msg}`,{
+                position: toast.POSITION.TOP_RIGHT
+            });
+            setModalUpdate(false);
+        } catch (error) {
+            let errMsg = 'Internal Server Error'
+            if (error.response?.status !== 500) {
+                errMsg = error.response?.data?.metadata?.msg
+            }
+            
+            toast.error(`Error ${error?.response?.status} - ${errMsg}`, {
+                position: toast.POSITION.TOP_RIGHT
+            });
+            setisLoading(false);
+        }
+    }
+    const handleSubmitAdd = async (e) => {
+        e.preventDefault();
+        setisLoading(true);
+        const form = e.target;
+        const formData = new FormData(form);
+        const formJSON = Object.fromEntries(formData.entries());
+        try {
+            const res = await axios.post(`${process.env.REACT_APP_API_HOST}/productCategories`, formJSON).then(res => res.data.metadata);
+            setisLoading(false);
+            toast.success(`${res?.msg}`,{
+                position: toast.POSITION.TOP_RIGHT
+            });
+            setModalAdd(false);
+        } catch (error) {
+            let errMsg = 'Internal Server Error'
+            if (error.response?.status !== 500) {
+                errMsg = error.response?.data?.metadata?.msg
+            }
+            
+            toast.error(`Error ${error?.response?.status} - ${errMsg}`, {
+                position: toast.POSITION.TOP_RIGHT
+            });
+            setisLoading(false);
+        }
+    }
+    useEffect(() => {
+      const getData = async () => {
+        try {
+            setisLoading(true);
+            const res = await axios.get(`${process.env.REACT_APP_API_HOST}/productCategories`).then((res) => res.data);
+            setProductCategories(res.data);
+            setisLoading(false);            
+           
+        } catch (error) {
+            let errMsg = 'Internal Server Error'
+            if (error.response?.status !== 500) {
+                errMsg = error.response?.data?.metadata?.msg
+            }
+            
+            toast.error(`Error ${error?.response?.status} - ${errMsg}`, {
+                position: toast.POSITION.TOP_RIGHT
+            });
+            setisLoading(false);
+        }
+
+      }
+    
+      getData();
+    }, [])
+    
   return (
     <>
-        <div className='flex justify-between'>
-            <div className='font-bold text-lg text-neutral'>N Kategori</div>
-            <div className='btn btn-primary hover:text-white'>
+         <BeatLoader
+            color={'#6419E6'}
+            loading={isLoading}
+            cssOverride={override}
+            size={20}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+        /> 
+        <ToastContainer/>
+        {
+            modalAdd ? 
+            <ModalProductCategory
+                header = 'Tambah Kategori'
+                action = {handleSubmitAdd}
+                isModalOpen = {setModalAdd}
+            />
+            : null
+        }
+        {
+             modalUpdate  ? 
+             <ModalProductCategory
+                id = {selectedCategoryId}
+                header = 'Edit Kategori'
+                action = {handleSubmitEdit}
+                isModalOpen = {setModalUpdate}
+                editableData = {productCategory}
+                editableAction = {setProductCategory}
+                /> : null
+        }
+        <div className='flex justify-between '>
+            <div className='font-bold text-lg text-neutral'>
+                {productCategories.length === 0 ? '0 ' : `${productCategories.length} `}
+                 Kategori
+            </div>
+            <div 
+            onClick={() => setModalAdd((prev) => prev ? false : true)}
+            className='btn btn-primary hover:text-white'>
                 Tambah Kategori
             </div>
         </div>
@@ -14,12 +190,32 @@ export default function ProductCategories() {
             <table className="table w-full">
                 <thead>
                 <tr>
-                    <th></th>
                     <th>Kategori</th>
                     <th>Aksi</th>
                 </tr>
                 </thead>
                 <tbody>
+                    {
+                        productCategories?.map((data, idx) => {
+                            return (
+                            <tr key={data.id}>
+                                <td>{data?.name}</td>
+                                <td className='flex'>
+                                <div className='cursor-pointer'
+                                    onClick={() => getCategoryById(data.id)}
+                                >
+                                        <FontAwesomeIcon icon={faPenToSquare} className="mr-4"></FontAwesomeIcon>
+                                </div>
+                                <div className='cursor-pointer'
+                                    onClick={() => handleDeleteCategory(data.id)}
+                                    >
+                                        <FontAwesomeIcon icon={faTrash} className=""></FontAwesomeIcon>
+                                </div>
+                                </td>
+                            </tr>
+                            )
+                        })
+                    }
                 </tbody>
             </table>
         </div>
