e0bad25e5163bb778bed69225cc42d5a1a245965
Rizki Susilo Ramadhan <rizkysr90@gmail.com>
2023-05-23 13:29:26 +0700

    modified struk

--- src/components/StrukPembelian.jsx
+++ src/components/StrukPembelian.jsx
@@ -1,60 +1,72 @@
-import React from 'react'
-import moment from 'moment';
-import 'moment/locale/id'
-import formatRupiah from '../utils/formatRupiah';
-moment.locale('id');
+import React from "react";
+import moment from "moment";
+import "moment/locale/id";
+import formatRupiah from "../utils/formatRupiah";
+moment.locale("id");
 
-function StrukPembelian({orders,trxId}) {
+function StrukPembelian({ orders, trxId }) {
   return (
     <>
-         <div className='store-name'>Rizki Plastik</div>
-            <div className='store-address'> Jl. Puri Cendana No.112, Sumberjaya, Kec. Tambun Sel., Kabupaten Bekasi, Jawa Barat 17510</div>
-            <div className='trx-info'>
-                <div className='trx-id'>ID : {trxId}</div>
-                <div className='trx-date'>Date : {moment(orders?.orders?.createdAt).format('DD MMM YYYY, HH:mm')}</div>
-                <div className='trx-cashier_name'>Kasir : {orders?.order?.User?.name}</div>
-            </div>
-            <div className='product-list'>
-                <div className='tabs-1'>
-                    <div className='no'>No</div>
-                    <div className='name'>Nama</div>
-                    <div className='qty'>Qty</div>
-                    <div className='price'>Harga</div>
-                    <div className='total'>Total</div>
-                </div>
-                    {
-                        orders?.order_details?.map((elm,idx) => {
-                            // let elm = elm.of_orders_details;
-                            const snap = elm.Snap_product;
+      <div className="store-name">Rizki Plastik</div>
+      <div className="store-address">
+        {" "}
+        Jl. Puri Cendana No.112, Sumberjaya, Kec. Tambun Sel., Kabupaten Bekasi,
+        Jawa Barat 17510
+      </div>
+      <div className="trx-info">
+        <div className="trx-id">ID : {trxId}</div>
+        <div className="trx-date">
+          Date :{" "}
+          {moment(orders?.orders?.createdAt).format("DD MMM YYYY, HH:mm")}
+        </div>
+        <div className="trx-cashier_name">
+          Kasir : {orders?.order?.User?.name}
+        </div>
+      </div>
+      <div className="product-list">
+        <div className="tabs-1">
+          <div className="no">No</div>
+          <div className="name">Nama</div>
+          <div className="qty">Qty</div>
+          <div className="price">Harga</div>
+          <div className="total">Total</div>
+        </div>
+        {orders?.order_details?.map((elm, idx) => {
+          // let elm = elm.of_orders_details;
+          const snap = elm.Snap_product;
 
-                            return (
-                                <div className='product' key={idx}>
-                                    <div className='no'>{idx+1}</div>
-                                    <div className='name'>{snap?.name}</div>
-                                    <div className='qty'>{elm.qty}</div>
-                                    <div className='price'>{formatRupiah(snap?.sell_price)}</div>
-                                    <div className='total'>{formatRupiah(elm.sum_price_each)}</div>
-                                </div>
-                            )
-                        })
-                    }
+          return (
+            <div className="product" key={idx}>
+              <div className="no">{idx + 1}</div>
+              <div className="name">{snap?.name}</div>
+              <div className="qty">{elm.qty}</div>
+              <div className="price">{formatRupiah(snap?.sell_price)}</div>
+              <div className="total">{formatRupiah(elm.sum_price_each)}</div>
             </div>
-            <div className='price-container'>
-                <div className='price_total pay'>
-                    <div>Harga Total : </div>
-                    <div>Rp{formatRupiah(orders?.orders?.amount)}</div>
-                </div>
-                <div className='pay_amount pay'>
-                    <div>Bayar : </div>
-                    <div>Rp{formatRupiah(orders?.orders?.pay_amount)}</div>
-                </div>
-                <div className='pay_change pay'>
-                    <div>Kembali : </div>
-                    <div>Rp{formatRupiah(Number(orders?.orders?.pay_amount) - Number(orders?.orders?.amount))}</div>
-                </div>
-            </div>
+          );
+        })}
+      </div>
+      <div className="price-container">
+        <div className="price_total pay">
+          <div>Harga Total : </div>
+          <div>Rp{formatRupiah(orders?.order?.amount)}</div>
+        </div>
+        <div className="pay_amount pay">
+          <div>Bayar : </div>
+          <div>Rp{formatRupiah(orders?.order?.pay_amount)}</div>
+        </div>
+        <div className="pay_change pay">
+          <div>Kembali : </div>
+          <div>
+            Rp
+            {formatRupiah(
+              Number(orders?.order?.pay_amount) - Number(orders?.order?.amount)
+            )}
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default StrukPembelian
+export default StrukPembelian;
--- src/pages/CashierTransactionSuccess.jsx
+++ src/pages/CashierTransactionSuccess.jsx
@@ -1,113 +1,125 @@
-import axios from 'axios';
-import React, { useEffect, useRef, useState } from 'react'
-import { useSelector } from 'react-redux'
-import { useNavigate, useParams } from 'react-router-dom';
-import { ClipLoader } from 'react-spinners';
-import ReactToPrint from 'react-to-print';
-import moment from 'moment';
-import { toast, ToastContainer } from 'react-toastify';
-import styles from './../styles/struk.module.css';
-import override from '../styles/spinner';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCheckCircle } from '@fortawesome/free-regular-svg-icons';
-import formatRupiah from '../utils/formatRupiah';
-import structStyles from '../styles/stringStruct';
-import StrukPembelian from '../components/StrukPembelian';
+import axios from "axios";
+import React, { useEffect, useRef, useState } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate, useParams } from "react-router-dom";
+import { ClipLoader } from "react-spinners";
+import ReactToPrint from "react-to-print";
+import moment from "moment";
+import { toast, ToastContainer } from "react-toastify";
+import styles from "./../styles/struk.module.css";
+import override from "../styles/spinner";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheckCircle } from "@fortawesome/free-regular-svg-icons";
+import formatRupiah from "../utils/formatRupiah";
+import structStyles from "../styles/stringStruct";
+import StrukPembelian from "../components/StrukPembelian";
 
 export default function CashierTransactionSuccess() {
-    const [isLoading, setIsLoading] = useState(false);
-    const {total, payAmount}  = useSelector(
-        (state) => state.cashier
-    );
-    const [orders, setOrders] = useState({});
-    let { trxId } = useParams();
-    const navigate = useNavigate();
-    const componentRef = useRef();
-    useEffect(() => {
-        const getData = async () => {
-            try {
-                setIsLoading(true)
-                const res = await axios.get(`${process.env.REACT_APP_API_HOST}/ofOrders/${trxId}`).then((res) => res.data.data);
-                setOrders(res);
-                setIsLoading(false)
-            } catch (error) {
-                let errFromServer = error?.response?.data?.metadata;
-                let errMsg = error.message;
-                if (error.response?.status !== 500) {
-                    if (errFromServer?.msg) {
-                        errMsg = errFromServer?.msg;
-                    } 
-                }
-                toast.error(`Error ${error?.response?.status} - ${errMsg}`);
-                setIsLoading(false);
-            }
+  const [isLoading, setIsLoading] = useState(false);
+  const { total, payAmount } = useSelector((state) => state.cashier);
+  const [orders, setOrders] = useState({});
+  console.log(orders);
+  let { trxId } = useParams();
+  const navigate = useNavigate();
+  const componentRef = useRef();
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        setIsLoading(true);
+        const res = await axios
+          .get(`${process.env.REACT_APP_API_HOST}/ofOrders/${trxId}`)
+          .then((res) => res.data.data);
+        setOrders(res);
+        setIsLoading(false);
+      } catch (error) {
+        let errFromServer = error?.response?.data?.metadata;
+        let errMsg = error.message;
+        if (error.response?.status !== 500) {
+          if (errFromServer?.msg) {
+            errMsg = errFromServer?.msg;
+          }
         }
-        getData()
-    }, [trxId])
+        toast.error(`Error ${error?.response?.status} - ${errMsg}`);
+        setIsLoading(false);
+      }
+    };
+    getData();
+  }, [trxId]);
   return (
-    <>  
-        {
-            isLoading && 
-            <div className='bg-base-100 fixed z-50 w-full left-0 top-0 right-0 min-h-screen'>
-              <ClipLoader
-              color={"#1eb854"}
-              loading={isLoading}
-              size={35}
-              cssOverride={override}
-              aria-label="Loading Spinner"
-              data-testid="loader"
-              />
-          </div>
-        }
-         <ToastContainer
-           autoClose={3000}
-           limit={1}
-           hideProgressBar={false}
-           newestOnTop={false}
-           closeOnClick={false}
-           rtl={false}
-           pauseOnFocusLoss={false}
-           draggable={false}
-           pauseOnHover
-           theme="dark"
+    <>
+      {isLoading && (
+        <div className="bg-base-100 fixed z-50 w-full left-0 top-0 right-0 min-h-screen">
+          <ClipLoader
+            color={"#1eb854"}
+            loading={isLoading}
+            size={35}
+            cssOverride={override}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+        </div>
+      )}
+      <ToastContainer
+        autoClose={3000}
+        limit={1}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss={false}
+        draggable={false}
+        pauseOnHover
+        theme="dark"
+      />
+      <div className={styles.con}>
+        <div className="con-page" ref={componentRef}>
+          <StrukPembelian orders={orders} trxId={trxId} />
+        </div>
+      </div>
+      <div className="flex flex-col items-center mt-4">
+        <FontAwesomeIcon
+          icon={faCheckCircle}
+          className="text-success text-5xl"
         />
-        <div className={styles.con}>
-            <div className='con-page' ref={componentRef}>
-                   <StrukPembelian orders={orders} trxId={trxId}/>
-            </div>
-        </div> 
-        <div className='flex flex-col items-center mt-4'>
-            <FontAwesomeIcon icon={faCheckCircle} className="text-success text-5xl"/>
-            <div className='font-bold text-xl mt-4'>Transaksi Berhasil</div>
-            <div>{moment(orders?.createdAt).format('Do MMMM YYYY, h:mm:ss a')}</div>
+        <div className="font-bold text-xl mt-4">Transaksi Berhasil</div>
+        <div>{moment(orders?.createdAt).format("Do MMMM YYYY, h:mm:ss a")}</div>
+      </div>
+      <div className="mt-4">
+        <div className="text-sm flex justify-between">
+          <div className="">Total Tagihan </div>
+          <div className="font-bold">Rp{formatRupiah(total)}</div>
+          {/* <div>Rp {payAmount - total}</div> */}
         </div>
-        <div className='mt-4'>
-            <div className='text-sm flex justify-between'>
-                <div className=''>Total Tagihan </div>
-                <div className='font-bold'>Rp{formatRupiah(total)}</div>
-                {/* <div>Rp {payAmount - total}</div> */}
-            </div>
-            <div className='text-sm flex justify-between mt-2'>
-                <div className=''>Dibayar</div>
-                <div className='font-bold'>Rp{formatRupiah(payAmount)}</div>
-                {/* <div className='font-bold'>Rp{ formatRupiah(payAmount - total)}</div> */}
-            </div>
-            <div className='text-sm flex justify-between mt-2'>
-                <div className=''>Kembalian</div>
-                <div className='font-bold text-warning'>Rp{ formatRupiah(payAmount - total)}</div>
-            </div>
+        <div className="text-sm flex justify-between mt-2">
+          <div className="">Dibayar</div>
+          <div className="font-bold">Rp{formatRupiah(payAmount)}</div>
+          {/* <div className='font-bold'>Rp{ formatRupiah(payAmount - total)}</div> */}
         </div>
-        <div className='mt-8 flex'>
-            <ReactToPrint
-                trigger={() => <div 
-                    className='btn btn-primary basis-6/12 btn-sm mr-1 rounded-lg normal-case btn-outline'> Cetak Struk</div>}
-                content={() => componentRef.current}
-                pageStyle={structStyles}
-            />
-            <div 
-            onClick={() => navigate('/admin/dashboard/cashier')}
-            className='btn btn-primary basis-6/12 btn-sm rounded-lg normal-case'>Transaksi Baru</div>
+        <div className="text-sm flex justify-between mt-2">
+          <div className="">Kembalian</div>
+          <div className="font-bold text-warning">
+            Rp{formatRupiah(payAmount - total)}
+          </div>
         </div>
+      </div>
+      <div className="mt-8 flex">
+        <ReactToPrint
+          trigger={() => (
+            <div className="btn btn-primary basis-6/12 btn-sm mr-1 rounded-lg normal-case btn-outline">
+              {" "}
+              Cetak Struk
+            </div>
+          )}
+          content={() => componentRef.current}
+          pageStyle={structStyles}
+        />
+        <div
+          onClick={() => navigate("/admin/dashboard/cashier")}
+          className="btn btn-primary basis-6/12 btn-sm rounded-lg normal-case"
+        >
+          Transaksi Baru
+        </div>
+      </div>
     </>
-  )
+  );
 }
